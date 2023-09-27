3ffd3aa8bbae20546618c54f50f3cbe2ab1231e6
Danny Lim <Danny.B.Lim@gmail.com>
2023-09-27 12:38:33 +0700

    MUI Theme font colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { grey, yellow } from "@mui/material/colors";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import Home from "./pages/Home";
@@ -7,6 +8,12 @@ import Home from "./pages/Home";
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: yellow[300],
+    },
+    secondary: {
+      main: grey[400],
+    },
   },
 });
 
