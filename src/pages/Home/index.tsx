d6411f8c56003f57c1d38c0078b9711f6e87a2b4
Danny Lim <Danny.B.Lim@gmail.com>
2023-09-27 11:58:25 +0700

    React Spring initial animation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,37 @@
 import React from "react";
+import { animated, useSpring } from "@react-spring/web";
 
 import SleekAppLogo from "../../assets/images/sleekapp-logo.png";
 
 function Home() {
+  const [springs, api] = useSpring(() => ({
+    from: { x: 100 },
+  }));
+
+  const handleClick = () => {
+    api.start({
+      from: {
+        x: 0,
+      },
+      to: {
+        x: 100,
+      },
+    });
+  };
+
   return (
     <div>
       <h1>Home</h1>
-      <img
+      <animated.img
         src={SleekAppLogo}
         alt="Sleek App"
         width={200}
-        style={{ width: 200, height: "auto" }}
+        onClick={handleClick}
+        style={{
+          width: 200,
+          height: "auto",
+          ...springs,
+        }}
       />
     </div>
   );
