cbd4dc3e129f006916e7afc33318ea00a44e488f
Danny Lim <Danny.B.Lim@gmail.com>
2023-09-27 12:10:01 +0700

    Shape hero component on front page

---
+++ src/pages/Home/index.styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+`;
+
+export const HeroWrapper = styled(Flex)`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  height: fit-content;
+  padding: 2.5%;
+`;
+
+export const HeroLeft = styled(Flex)`
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: calc(100vw - 400px);
+  height: fit-content;
+  text-align: left;
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { animated, useSpring } from "@react-spring/web";
 
+// Styles
+import { HeroWrapper, HeroLeft } from "./index.styles";
+
+// Images
 import SleekAppLogo from "../../assets/images/sleekapp-logo.png";
 
 function Home() {
@@ -11,28 +15,38 @@ function Home() {
   const handleClick = () => {
     api.start({
       from: {
-        x: 0,
+        x: -200,
       },
       to: {
-        x: 100,
+        x: -100,
       },
     });
   };
 
   return (
     <div>
-      <h1>Home</h1>
-      <animated.img
-        src={SleekAppLogo}
-        alt="Sleek App"
-        width={200}
-        onClick={handleClick}
-        style={{
-          width: 200,
-          height: "auto",
-          ...springs,
-        }}
-      />
+      <HeroWrapper>
+        <HeroLeft>
+          <h1>Sleek App</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum cum
+            nam obcaecati quia ea facere, consequuntur cupiditate beatae aut
+            aspernatur saepe quis assumenda omnis corporis voluptas sit
+            consequatur deserunt ipsam?
+          </p>
+        </HeroLeft>
+        <animated.img
+          src={SleekAppLogo}
+          alt="Sleek App"
+          width={300}
+          onClick={handleClick}
+          style={{
+            width: 300,
+            height: "auto",
+            ...springs,
+          }}
+        />
+      </HeroWrapper>
     </div>
   );
 }
