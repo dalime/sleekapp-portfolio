fb066b4615e0b411cf25000e487beddcb2944d64
Danny Lim <Danny.B.Lim@gmail.com>
2023-09-28 17:59:38 +0700

    Navigation menu gray color background

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -50,7 +50,7 @@ function HeaderMenu() {
       data-testid="desktop-navigation"
       className="navbar"
       style={{
-        background: `linear-gradient(to bottom, rgba(0, 0, 0, ${opacity}) 0%, rgba(0, 0, 0, ${opacity}) 100%)`,
+        background: `linear-gradient(to bottom, rgba(14, 14, 14, ${opacity}) 0%, rgba(14, 14, 14, ${opacity}) 100%)`,
       }}
     >
       <LeftSide>
