e17ca44e72e9779273a63a8a67e9664da4ab1da3
Danny Lim <Danny.B.Lim@gmail.com>
2023-09-28 15:47:18 +0700

    Colors in hero and button functionaity

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -3,6 +3,9 @@ import { animated, useSpring } from "@react-spring/web";
 import { Button, Typography } from "@mui/material";
 import { yellow } from "@mui/material/colors";
 
+// Helpers
+import { navigateToUrl } from "../../helpers";
+
 // Styles
 import { HeroWrapper, HeroLeft } from "./index.styles";
 
@@ -37,15 +40,22 @@ function Hero() {
           }}
         >
           Sleek App{" "}
-          <span style={{ color: yellow[500] }}>Development Agency</span>
+          <span style={{ color: yellow[200] }}>Development Agency</span>
         </Typography>
-        <Typography variant="h2" sx={{ fontWeight: 500 }}>
-          Let's Bring Your Vision to Life
+        <Typography variant="h2" sx={{ fontWeight: 500, marginTop: 2 }}>
+          Let's Bring Your Vision to{" "}
+          <span style={{ color: yellow[500] }}>Life</span>
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" sx={{ marginTop: 2 }}>
           A One-Stop-Shop App Development Agency. We turn visions into reality.
         </Typography>
-        <Button variant="contained" sx={{ marginTop: 4 }}>
+        <Button
+          variant="contained"
+          sx={{ marginTop: 4 }}
+          onClick={() =>
+            navigateToUrl("https://calendly.com/sleekapp/consultation")
+          }
+        >
           Develop Your App
         </Button>
       </HeroLeft>
