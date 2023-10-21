ec852796fb4817f62dfdd7bbe30c22014a97b9e4
dalime <Danny.B.Lim@gmail.com>
2023-10-21 20:35:33 +0700

    Set up express first route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express, { Request, Response } from "express";
+
+// Constants
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT ${PORT}`);
+});
