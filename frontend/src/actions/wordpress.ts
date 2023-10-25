4b7f7fcf2c162e256d38ab84a01027ee9a762e0c
dalime <Danny.B.Lim@gmail.com>
2023-10-25 14:36:07 +0700

    Edit console logs for team member posts fetch

--- frontend/src/actions/wordpress.ts
+++ frontend/src/actions/wordpress.ts
@@ -39,10 +39,10 @@ const fetchTeamPosts = async (): Promise<WPPost[]> => {
     // eslint-disable-next-line no-useless-escape
     const fixedRes = res.replace(/\<\? header\(\"Access-Control-Allow-Origin: \*\"\); \?>/g, "");
     const jsonRes = JSON.parse(fixedRes);
-    console.log("Fetched agency posts", jsonRes);
+    console.log("Fetched team members' posts", jsonRes);
     return jsonRes as WPPost[];
   }).catch((error) => {
-    console.error("Error fetching agency posts", error);
+    console.error("Error fetching team members' posts", error);
     return [];
   });
 };
