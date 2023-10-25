92b5c7444f4d9fc0c7c4f47b1128f9542efde167
dalime <Danny.B.Lim@gmail.com>
2023-10-25 14:35:30 +0700

    Replaced wpapi calls with fetch to fix cors issues

--- frontend/src/actions/wordpress.ts
+++ frontend/src/actions/wordpress.ts
@@ -1,12 +1,51 @@
-import WPAPI from 'wpapi';
-
 // Types
 import { WPPost } from '../types';
 
-// Initialize WPAPI
-const wpAgency = new WPAPI({ endpoint: `${process.env.REACT_APP_AGENCY_WP_URL}/wp-json` });
-// Initialize WPAPI
-const wpPersonal = new WPAPI({ endpoint: `${process.env.REACT_APP_PERSONAL_WP_URL}/wp-json` });
+/**
+ * Fetches the blog posts associated with the agency WordPress
+ * @returns WPPost[]
+ */
+const fetchAgencyPosts = async (): Promise<WPPost[]> => {
+  return fetch(`${process.env.REACT_APP_AGENCY_WP_URL}/wp-json/wp/v2/posts`, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.text()).then((res) => {
+    // eslint-disable-next-line no-useless-escape
+    const fixedRes = res.replace(/\<\? header\(\"Access-Control-Allow-Origin: \*\"\); \?>/g, "");
+    const jsonRes = JSON.parse(fixedRes);
+    console.log("Fetched agency posts", jsonRes);
+    return jsonRes as WPPost[];
+  }).catch((error) => {
+    console.error("Error fetching agency posts", error);
+    return [];
+  });
+};
+
+/**
+ * Fetches the blog posts associated with the team members' WordPress websites
+ * @returns WPPost[]
+ */
+const fetchTeamPosts = async (): Promise<WPPost[]> => {
+  return fetch(`${process.env.REACT_APP_PERSONAL_WP_URL}/wp-json/wp/v2/posts`, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.text()).then((res) => {
+    // eslint-disable-next-line no-useless-escape
+    const fixedRes = res.replace(/\<\? header\(\"Access-Control-Allow-Origin: \*\"\); \?>/g, "");
+    const jsonRes = JSON.parse(fixedRes);
+    console.log("Fetched agency posts", jsonRes);
+    return jsonRes as WPPost[];
+  }).catch((error) => {
+    console.error("Error fetching agency posts", error);
+    return [];
+  });
+};
 
 /**
  * Fetches blog posts from WP backend
@@ -14,9 +53,12 @@ const wpPersonal = new WPAPI({ endpoint: `${process.env.REACT_APP_PERSONAL_WP_UR
  */
 export const fetchBlogPosts = async (): Promise<WPPost[] | null> => {
   return new Promise(async (resolve, reject) => {
+    console.log(`${process.env.REACT_APP_AGENCY_WP_URL}/wp-json`);
+    console.log(`${process.env.REACT_APP_PERSONAL_WP_URL}/wp-json`);
+
     try {
-      const agencyPosts = await wpAgency.posts();
-      const personalPosts = await wpPersonal.posts();
+      const agencyPosts: WPPost[] = await fetchAgencyPosts();
+      const personalPosts: WPPost[] = await fetchTeamPosts();
       const blogPosts = [ ...agencyPosts, ...personalPosts ];
       resolve(blogPosts);
     } catch (err) {
